93e9af59337b8a3ad563f05c7c8fb28c13cfd307
Tehseen <tehsu@snyde.net>
2023-07-05 01:13:51 -0500

    Update master_nodes.tf

--- master_nodes.tf
+++ master_nodes.tf
@@ -86,7 +86,7 @@ resource "proxmox_vm_qemu" "k3s-master" {
   connection {
     type = "ssh"
     user = local.master_node_settings.user
-    private_key = file("~/.ssh/id_rsa")
+    private_key = var.private_key
     host = local.master_node_ips[count.index]
   }
 
